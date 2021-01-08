f62d2d3344af608e47c6bbedc4fd88a54cd5d728
Gabriel Oliveira <gosilva.pro@gmail.com>
2021-01-07 22:48:00 -0300

    Add home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Home from './Home';
 import Navbar from './NavBar';
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
        <Navbar />
+       <Home />
     </div>
   );
 }
